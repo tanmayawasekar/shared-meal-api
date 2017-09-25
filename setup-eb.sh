6ff2cbb502455efb3ba3c1f2ff85fdf9b7cfb7be
Tanmay Awasekar <tanmayawasekar@gmail.com>
2017-09-26 00:09:06 +0530

    try

--- setup-eb.sh
+++ setup-eb.sh
@@ -4,6 +4,6 @@ set -e
 sudo mkdir -p /home/ubuntu/.aws
 sudo touch /home/ubuntu/.aws/config
 sudo chmod 600 /home/ubuntu/.aws/config
-echo "[profile eb-cli]" > /home/ubuntu/.aws/config
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
+sudo echo "[profile eb-cli]" > /home/ubuntu/.aws/config
+sudo echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
+sudo echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
